c1b3503b53255cea917b1d9057f7678c380c4cdf
Joao Victor <joaobrvictor@gmail.com>
2019-06-10 17:10:00 -0300

    Search bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { plants } from './plants';
 
 ReactDOM.render(
-    <CardList plants={plants} />
+    <App />
     , document.getElementById('root'));
 serviceWorker.unregister();
