fa63c86723b3e2e08ab9acb3d49846253dd502b8
Joao Victor <joaobrvictor@gmail.com>
2019-06-11 12:41:29 -0300

    Updates

---
+++ precache-manifest.d9bdb260b007d24bf99b803cc3037c53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899e7444bbb37de7001f71e5a918736b",
+    "url": "/vixtorocha/plantfriends/index.html"
+  },
+  {
+    "revision": "c6c4e6094f4918df0546",
+    "url": "/vixtorocha/plantfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ec60718418f85dab8e0d",
+    "url": "/vixtorocha/plantfriends/static/css/main.32a0ae71.chunk.css"
+  },
+  {
+    "revision": "c6c4e6094f4918df0546",
+    "url": "/vixtorocha/plantfriends/static/js/2.0ef9ac0a.chunk.js"
+  },
+  {
+    "revision": "ec60718418f85dab8e0d",
+    "url": "/vixtorocha/plantfriends/static/js/main.48e5b7d6.chunk.js"
+  },
+  {
+    "revision": "680b7868f810411b5787",
+    "url": "/vixtorocha/plantfriends/static/js/runtime~main.5fa2fe5d.js"
+  },
+  {
+    "revision": "6a7f2002170d8dcbd8f3a1f6bfbfe6b8",
+    "url": "/vixtorocha/plantfriends/static/media/peacelily.6a7f2002.jpg"
+  },
+  {
+    "revision": "ebcfa5069d3ed814e9f82d0197b17353",
+    "url": "/vixtorocha/plantfriends/static/media/pothos.ebcfa506.jpg"
+  },
+  {
+    "revision": "ac885e5c50335a3eca4a934ffc4853f2",
+    "url": "/vixtorocha/plantfriends/static/media/zz-plant.ac885e5c.jpg"
+  }
+]);
