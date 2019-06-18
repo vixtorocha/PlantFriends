2aec0142af93f2228f6b8f12eda14eca4280cae5
Joao Victor <joaobrvictor@gmail.com>
2019-06-18 16:53:49 -0300

    More plants for good visualization

--- src/plants.js
+++ src/plants.js
@@ -1,5 +1,4 @@
-export const plants = [
-    {
+export const plants = [{
         id: 1,
         name: 'ZZ-Plant',
         link: require('../src/imgs/zz-plant.jpg')
@@ -13,5 +12,61 @@ export const plants = [
         id: 3,
         name: 'Pothos',
         link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 4,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 5,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 6,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 7,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 8,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 9,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 10,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 11,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 12,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 13,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
+    },
+    {
+        id: 14,
+        name: 'Pothos',
+        link: require('../src/imgs/pothos.jpg')
     }
+
 ]
